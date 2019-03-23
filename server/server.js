71f931f4213dcf8ac98b5110d0a01665a6314623
suresh <sureshsunsuri@gmail.com>
2019-03-23 18:25:52 +0530

    added POST /hospital/create

--- server/server.js
+++ server/server.js
@@ -8,10 +8,7 @@ const app = express();
 
 // mongoose middleware
 mongoose
-  .connect(
-    'mongodb+srv://onload:onload@cluster0-90jfs.mongodb.net/test?retryWrites=true',
-    {useNewUrlParser: true}
-  )
+  .connect('mongodb://localhost:27017/hospital', {useNewUrlParser: true})
   .then(() => console.log('connected to database'))
   .catch(err => console.log(err));
 
@@ -31,6 +28,10 @@ app.use(
     saveUninitialized: true
   })
 );
+
+const hospital = require('./routers/hospital');
+
+app.use('/admin', hospital);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
