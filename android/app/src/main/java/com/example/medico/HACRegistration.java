1d8d00f874ee27de35f1574d666d6ba62f41933a
Ruthvikbr <ruthvikbr24@gmail.com>
2019-03-23 11:50:21 +0530

    USER and HCA registration added

--- android/app/src/main/java/com/example/medico/HACRegistration.java
+++ android/app/src/main/java/com/example/medico/HACRegistration.java
@@ -1,13 +1,52 @@
 package com.example.medico;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class HACRegistration extends AppCompatActivity {
+private TextView HAC_BDAY_TV ;
+static  int year,day,month;
 
-    @Override
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hacregistration);
+        HAC_BDAY_TV = findViewById(R.id.HAC_BDAY_TV);
     }
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+             year = c.get(Calendar.YEAR);
+             month = c.get(Calendar.MONTH);
+             day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
+    }
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+        String bday = day+"/"+month+"/"+year;
+        HAC_BDAY_TV.setText(""+bday);
+
+
+    }
+
 }
--- android/app/src/main/java/com/example/medico/LoginActivity.java
+++ android/app/src/main/java/com/example/medico/LoginActivity.java
@@ -2,12 +2,20 @@ package com.example.medico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    public EditText UserMail_ET , UserPWD_ET;
+    public Button USERSubmit;
+    private String mailID, password;
+    private boolean success = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        UserMail_ET = findViewById(R.id.USER_MAIL_ET);
+        UserPWD_ET = findViewById(R.id.USER_PASSWORD_ET);
     }
 }
