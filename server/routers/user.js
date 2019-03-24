20cbbb49a6fb37e68c1e11d12111ada3c58ebedb
tejas100 <tejasastendulker@gmail.com>
2019-03-24 14:36:07 +0530

    ädded user router

---
+++ server/routers/user.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('')
+
+const router  = express.Router()
+
+require('../models/user')
+const Doctor = mongoose.model('doctor')
+
+router.get('/doctor', (req, res) =>{
+
+    Doctor.find({}).then(doctor => {
+        res.json(doctor)
+    }).catch(err => console.log(err))
+        
+})
+
+module.exports = router
